29aef98ca715299d02b310e24ac52744c2077d09
Lucascheetah <Lucassmedengaard@gmail.com>
2025-11-27 12:49:54 +0100

    added 2 classes
    Tilføjet klasserne Boat og BoatReg

---
+++ ClassLibSailClub/Boat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibSailClub
+{
+    public class Boat
+    {
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Name { get; set; }
+        public int SailNum { get; set; }
+        public string MotorInf { get; set; }
+        public double Length { get; set; }
+
+        public Boat(string type, string model, int year, string name, int sailNum, string motorInf, double length)
+        {
+            Type = type;
+            Model = model;
+            Name = name;
+            SailNum = sailNum;
+            MotorInf = motorInf;
+            Length = length;
+        }
+
+
+    }
+}
---
+++ ClassLibSailClub/BoatReg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibSailClub
+{
+    public class BoatReg
+    {
+        List<Boat> Boats { get; set; } = new List<Boat>
+        {
+
+        };
+        public BoatReg()
+        {
+
+        }
+
+        public void AddBoat(string type, string model, int year, string name, int sailNum, string motorInf, double length)
+        {
+            Boat tempObj = new Boat(type, model, year, name, sailNum, motorInf, length);
+            Boats.Add(tempObj);
+        }
+
+        public void DelBoat(int sailNum)
+        {
+            foreach (Boat boat in Boats)
+            {
+                if (boat.SailNum == sailNum)
+                {
+                    Boats.Remove(boat);
+                }
+            }
+        }
+
+        public void UpdBoat(int sailNum)
+        {
+
+        }
+
+        public void ReadBoat(int sailNum)
+        {
+
+        }
+    }
+
+}
