5dabfdc6af039da6800e98189ca5b0f1f47d55d8
Paw Møller <pawmoeller94@gmail.com>
2025-11-28 10:34:31 +0100

    Add Calendar and Event classes
    Introduces the Calendar class with lists for bookings and events, and a method to create new events. Adds the Event class with properties for event details and methods to manage participants.

---
+++ ClassLibSailClub/Event.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibSailClub
+{
+    public class Event
+    {
+        public string Navn { get; set; } //Properties
+        public string Dato { get; set; }
+        public string Tid { get; set; }
+        public int Pris { get; set; }
+        public string Beskrivelse { get; set; }
+        public List<Medlem> DeltagerListe { get; set; }
+
+        public Event(string navn, string dato, string tid, int pris, string beskrivelse)
+
+        {
+            Navn = navn; //Constructor
+            Dato = dato;
+            Tid = tid;
+            Pris = pris;
+            Beskrivelse = beskrivelse;
+            DeltagerListe = new List<Medlem>();
+        }
+
+        public void AddDeltager(Medlem deltager) //Method
+        {
+            DeltagerListe.Add(deltager);
+        }
+
+        public void RemoveDeltager(Medlem deltager)
+        {
+            DeltagerListe.Remove(deltager);
+        }
+
+    }
+}
