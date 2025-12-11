35b2cf393c9682664a2151a731686e9973d6d299
Lucascheetah <Lucassmedengaard@gmail.com>
2025-12-11 09:23:31 +0100

    Delt båd op i 3

---
+++ ClassLibSailClub/SailBoat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibSailClub
+{
+    public class SailBoat : Boat
+    {
+        public SailBoat(string model, int year, string name, int sailNum, int capacity, double length)
+        : base(model, year, name, sailNum, capacity, length)
+        {
+
+        }
+        
+            public int NumberOfSails { get; set; }
+            public double SailSizeM2 { get; set; }
+        
+        public SailBoat(string model, int year, string name, int sailNum, int capacity, double length, int numberOfSails, double sailSizeM2)
+        {
+            NumberOfSails = numberOfSails;
+            SailSizeM2 = sailSizeM2;
+    }
+    }
+           
+
+    }
+
+
